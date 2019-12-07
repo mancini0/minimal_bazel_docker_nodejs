72b076251179d0b4490bd312300d4a6cb7cc856d
mancini <mancini0@gmail.com>
2019-12-07 11:46:22 -0600

    initial commit

---
+++ myjsstuff/App.js
@@ -0,0 +1,8 @@
+// var greeter = require 'Greeter';
+// greeter.greet();
+// OR
+// import greet from 'Greeter'
+// greet()
+
+
+console.log("Hello there my friend!");
---
+++ myjsstuff/Greeter.ts
@@ -0,0 +1,3 @@
+//your homework is to figure out how to import this ts library into your js app :)
+export default () => console.log("hello there friend");
+
